a75300b1b19efb8b2b00e729be558adf02d0ad62
Brian Artiaco <brian@kingofweb.com>
2012-04-09 11:23:34 -0700

    Refactored admin controllers so that all are now inheriting from the BaseController and are now properly protected actions.

--- app/controllers/forem/admin/members_controller.rb
+++ app/controllers/forem/admin/members_controller.rb
@@ -1,18 +1,20 @@
 module Forem
-  class Admin::MembersController < ApplicationController
+  module Admin
+    class MembersController < BaseController
 
-    def create
-      user = Forem.user_class.where(Forem.autocomplete_field => params[:user]).first
-      unless group.members.exists?(user.id)
-        group.members << user
+      def create
+        user = Forem.user_class.where(Forem.autocomplete_field => params[:user]).first
+        unless group.members.exists?(user.id)
+          group.members << user
+        end
+        render :status => :ok
       end
-      render :status => :ok
-    end
 
-    private
+      private
 
-    def group
-      @group ||= Forem::Group.find(params[:group_id])
+      def group
+        @group ||= Forem::Group.find(params[:group_id])
+      end
     end
   end
 end
--- app/controllers/forem/admin/users_controller.rb
+++ app/controllers/forem/admin/users_controller.rb
@@ -1,8 +1,11 @@
 module Forem
-  class Admin::UsersController < ApplicationController
-    def autocomplete
-      users = Forem.user_class.forem_autocomplete(params[:term])
-      render :json => users.map { |u| u.send(Forem.autocomplete_field) }
+  module Admin
+    class UsersController < BaseController
+      def autocomplete
+        users = Forem.user_class.forem_autocomplete(params[:term])
+        render :json => users.map { |u| u.send(Forem.autocomplete_field) }
+      end
     end
   end
 end
+
